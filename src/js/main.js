0325a3570255856bf3201e9eb8329ed23aaae48a
Yakup <yakupjoraev@gmail.com>
2022-08-16 12:21:28 +0300

    -

--- src/js/main.js
+++ src/js/main.js
@@ -444,3 +444,55 @@ function ourLifeSlider() {
 }
 
 ourLifeSlider();
+
+function progressPage() {
+  const container = document.querySelector('.progress')
+
+  if (!container) {
+    return neull
+  }
+
+  let SwiperTop = new Swiper('.swiper--top', {
+    spaceBetween: 0,
+    centeredSlides: true,
+    speed: 6000,
+    autoplay: {
+      delay: 1,
+    },
+    loop: true,
+    slidesPerView:'auto',
+    allowTouchMove: false,
+    disableOnInteraction: true
+  });
+
+  let SwiperMiddle = new Swiper('.swiper--middle', {
+    spaceBetween: 0,
+    centeredSlides: true,
+    speed: 6000,
+    autoplay: {
+      delay: 1,
+    },
+    loop: true,
+    slidesPerView:'auto',
+    allowTouchMove: false,
+    disableOnInteraction: true
+  });
+  
+  let SwiperBottom = new Swiper('.swiper--bottom', {
+    spaceBetween: 0,
+    centeredSlides: true,
+    speed: 6000,
+    autoplay: {
+      delay: 1,
+      reverseDirection: true
+    },
+    loop: true,
+    loopedSlides: 4,
+    slidesPerView:'auto',
+    allowTouchMove: false,
+    disableOnInteraction: true
+  });
+  
+}
+
+progressPage();
