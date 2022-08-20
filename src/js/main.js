355843afc28ce41c36b908ea24da9d950860e7bd
Yakup <yakupjoraev@gmail.com>
2022-08-20 15:53:43 +0300

    added new files/ finished progress page

--- src/js/main.js
+++ src/js/main.js
@@ -452,48 +452,53 @@ function progressPage() {
     return null
   }
 
-  let SwiperTop = new Swiper('.swiper--top', {
-    spaceBetween: 0,
-    centeredSlides: true,
-    speed: 6000,
-    autoplay: {
-      delay: 1,
-    },
-    loop: true,
-    slidesPerView:'auto',
-    allowTouchMove: false,
-    disableOnInteraction: true
-  });
-  
-  let SwiperMiddle = new Swiper('.swiper--middle', {
-    spaceBetween: 0,
-    centeredSlides: true,
-    speed: 6000,
-    autoplay: {
-      delay: 1,
-      reverseDirection: true
-    },
-    loop: true,
-    loopedSlides: 4,
-    slidesPerView:'auto',
-    allowTouchMove: false,
-    disableOnInteraction: true
-  });
+// Params
+var sliderSelector = '.progress-slider',
+    options = {
+      init: false,
+      loop: true,
+      speed:800,
+      slidesPerView: 1, // or 'auto'
+      // spaceBetween: 10,
+      centeredSlides : true,
+      effect: 'coverflow', // 'cube', 'fade', 'coverflow',
+      coverflowEffect: {
+        rotate: 50, // Slide rotate in degrees
+        stretch: 0, // Stretch space between slides (in px)
+        depth: 100, // Depth offset in px (slides translate in Z axis)
+        modifier: 1, // Effect multipler
+        slideShadows : true, // Enables slides shadows
+      },
+      grabCursor: true,
+      parallax: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        992: {
+          slidesPerView: 1,
+          centeredSlides : true,
+          spaceBetween: 0
+        }
+      },
+      // Events
+      on: {
+        imagesReady: function(){
+          this.el.classList.remove('loading');
+        }
+      }
+    };
+  var mySwiper = new Swiper(sliderSelector, options);
 
-  let SwiperBottom = new Swiper('.swiper--bottom', {
-    spaceBetween: 0,
-    centeredSlides: true,
-    speed: 6000,
-    autoplay: {
-      delay: 1,
-    },
-    loop: true,
-    slidesPerView:'auto',
-    allowTouchMove: false,
-    disableOnInteraction: true
-  });
-  
+  // Initialize slider
+  mySwiper.init();
   
 }
 
 progressPage();
+
